2af7ce2d91dee319de8d59c5799f65153dfbb1ce
Mina Ashna <mia@impact.dk>
2021-11-08 22:23:01 +0100

    initial commit

---
+++ Flager/FlagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagerApp.swift
+//  Flager
+//
+//  Created by Mina Ashna on 08/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct FlagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
